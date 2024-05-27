f98001f6450cbab14f74888e00a30bdb3fc4d8c9
John Grassey <johngrassey@gmail.com>
2024-05-27 17:10:37 -0400

    Generate computer turn
    Created a separate javascript page to store the code for this program.
    Computer should randomly generate a rock, paper, scissors result.

---
+++ script.js
@@ -0,0 +1,35 @@
+// Create function called getComputerChoice
+
+function getComputerChoice() {
+
+    // Create variable computerChoice
+    let computerChoice;
+
+    // Create variable randomComputerNumber
+    // Generate Random Number between 0 and 1, multiply by 3, and round down to nearest integer. Assign to variable randomComputerNumber
+    let randomComputerNumber = Math.floor((Math.random() * 3));
+
+    // If randomComputerNumber = 0, set computerChoice to "Rock"
+    if (randomComputerNumber === 0) {
+        computerChoice = "Rock";
+
+    // If randomComputerNumber = 1, set computerChoice to "Paper"
+
+    } else if (randomComputerNumber === 1){
+        computerChoice = "Paper";
+
+    // If randomComputerNumber = 2, set computerChoice to "Scissors"   
+
+    } else {
+        computerChoice = "Scissors";
+        
+    };
+
+    // Return computerChoice
+    return computerChoice
+
+};
+
+
+// Print computerChoice to console
+console.log(getComputerChoice());
