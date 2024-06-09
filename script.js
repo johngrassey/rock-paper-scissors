96fa9430d9b4e083331560f2189f52211c41dbb5
John Grassey <johngrassey@gmail.com>
2024-06-09 18:11:49 -0400

    Remove Unecessary Comments
    I learned that too many comments make your code difficult to read.

--- script.js
+++ script.js
@@ -2,53 +2,29 @@
 
 function getComputerChoice() {
 
-    // Create variable computerChoice
     let computerChoice;
-
-    // Create variable randomComputerNumber
-    // Generate Random Number between 0 and 1, multiply by 3, and round down to nearest integer. Assign to variable randomComputerNumber
     let randomComputerNumber = Math.floor((Math.random() * 3));
 
-    // If randomComputerNumber = 0, set computerChoice to "Rock"
     if (randomComputerNumber === 0) {
         computerChoice = "rock";
-
-    // If randomComputerNumber = 1, set computerChoice to "Paper"
-
     } else if (randomComputerNumber === 1){
         computerChoice = "paper";
-
-    // If randomComputerNumber = 2, set computerChoice to "Scissors"   
-
     } else {
         computerChoice = "scissors";
-        
     };
 
-    // Return computerChoice
     return computerChoice
 }
 
-// Create function called getHumanChoice
 function getHumanChoice() {
 
-    // Ask the user to "Rock, Paper, Scissors?" store it in new variable, humanChoice
     let humanChoice = prompt("Rock, Paper, Scissors?","");
-
-
-
-    // Return variable humanChoice
     return humanChoice;
 }
 
-// Create function called playRound with two inputs, humanChoice and computerChoice
 function playRound(humanChoice, computerChoice) {
 
-//Convert humanChoice to lowercase.
-
     humanChoice = String(humanChoice).toLowerCase();
-
-//Scoring logic.
 
     if (humanChoice === "rock") {
         if (computerChoice === "rock") {
@@ -110,7 +86,6 @@ function playGame() {
 
 }
 
-// Create variable humanScore and computerScore
 let humanScore = 0;
 let computerScore = 0;
 
